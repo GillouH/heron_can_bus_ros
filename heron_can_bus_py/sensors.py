d122b6f14746fcc18d40202d7eb835d5dbd315ca
Gillou100 <gilles.hubert@isen.yncrea.fr>
2019-11-29 10:32:41 +0100

    Update sensors.py

--- heron_can_bus_py/sensors.py
+++ heron_can_bus_py/sensors.py
@@ -2,26 +2,55 @@
 # coding: utf-8
 
 
-#from usb_can_analyzer.useful import ImplementError
+from abc import ABCMeta, abstractmethod
+from typing import Tuple
 
 
-class Sensor:
-    def __init__(self, ID: int):
-        self.ID = ID
-#    def manageMsg(self, msgType: int, serviceID: int, msgPayload: str):
-#        raise ImplementError("Not implemented method")
+class EDUCATSensor(metaclass=ABCMeta):
+    @staticmethod
+    def compactedMsgID(serviceID: int, nodeID: int) -> int:
+        return (serviceID << 6) + nodeID
 
-class IRSensor(Sensor):
-    def __init__(self, ID: int):
-        super().__init__(ID)
-#    def manageMsg(self, msgType: int, serviceID: int, msgPayload: str):
-#        pass
+    @staticmethod
+    def decompactedMsgID(msgID: int) -> Tuple[int, int]:
+        serviceID = msgID >> 6
+        nodeID = msgID - (serviceID << 6)
+        return serviceID, nodeID
 
-class IRUSSensor(Sensor):
-    def __init__(self, ID: int):
-        super().__init__(ID)
-#    def manageMsg(self, msgType: int, serviceID: int, msgPayload: str):
-#        pass
+    def __init__(self, ID : int):
+    	self.ID = ID
+    	self.distance = []
+
+    @abstractmethod
+    def manageMsg(self, msgType: int, serviceID: int, msgPayload: bytes):
+        pass
+
+    def getDistance(self):
+        return self.distance
+
+
+class IR_EDUCATSensor(EDUCATSensor):
+    def manageMsg(self, msgType: int, serviceID: int, msgPayload: bytes):
+        super().manageMsg(msgType, serviceID, msgPayload)
+        #if (serviceID == 2):
+        self.distance = [int(msgPayload[0:2].hex(), base=16),
+                        msgPayload[2],
+                        msgPayload[3],
+                        msgPayload[4],
+                        msgPayload[5]
+        ]
+
+
+class IRUS_EDUCATSensor(EDUCATSensor):
+    def manageMsg(self, msgType: int, serviceID: int, msgPayload: bytes):
+        super().manageMsg(msgType, serviceID, msgPayload)
+        #if (serviceID == 2):
+        self.distance = [int(msgPayload[0:2].hex(), base=16),
+                        int(msgPayload[2:4].hex(), base=16),
+                        msgPayload[4],
+                        msgPayload[5],
+                        msgPayload[6]
+        ]
 
 
 if __name__ == "__main__":
