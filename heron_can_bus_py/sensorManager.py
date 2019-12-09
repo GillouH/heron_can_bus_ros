7dfb0c5d8088c3a6129a5afbc7c0be568e4aae24
Gillou100 <gilles.hubert@isen.yncrea.fr>
2019-12-09 13:59:29 +0100

    présentation 2

--- heron_can_bus_py/sensorManager.py
+++ heron_can_bus_py/sensorManager.py
@@ -61,7 +61,7 @@ class SensorManager():
             self.initNode()
 
         def initNode(self):
-            period =  format(self.period, "x")
+            period = format(int(self.period * 1000), "x")
             if (len(period) % 2) != 0 : period = "0" + period
             for nodeID in self.sensors.keys():
                 self.converter.sendMessage(Converter.DATA, EDUCATSensor.compactedMsgID(1, nodeID), period)
